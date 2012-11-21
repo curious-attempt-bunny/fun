d3e7f9f4df270241f76cc7bf88df502b7e58799a
Merlyn Albery-Speyer <merlyn@newrelic.com>
2012-11-21 15:17:41 -0800

    Updated the requestbin

--- launch.sh
+++ launch.sh
@@ -3,5 +3,5 @@
 git stash save
 git pull --rebase
 echo "Yo!" >> ~/.fun/fun.txt
-curl -X POST http://requestb.in/1977ym41 -d user=$USER
+curl -X POST http://requestb.in/y3tld1y3 -d user=$USER
 open 'https://expresslane.apple.com/GetproductgroupList.action?locale=en_US&caller=cup'
