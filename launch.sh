572914df450933302b87a22ecac8c9035e4bf2f8
Merlyn <curious.attempt.bunny@gmail.com>
2013-01-07 16:49:29 -0800

    Mute volume

--- launch.sh
+++ launch.sh
@@ -5,3 +5,4 @@ git pull --rebase
 echo "Yo!" >> ~/.fun/fun.txt
 curl -X POST http://requestb.in/y3tld1y3 -d user=$USER
 open 'https://expresslane.apple.com/GetproductgroupList.action?locale=en_US&caller=cup'
+osascript -e "set Volume 0" # thx, dcelis
