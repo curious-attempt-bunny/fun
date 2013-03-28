29fa5f5e6b0d212689cf95e14e2d75414d3bd555
Merlyn Albery-Speyer <merlyn@newrelic.com>
2013-03-28 16:11:48 -0700

    Made images flip :>

--- launch.sh
+++ launch.sh
@@ -3,3 +3,6 @@
 git stash save
 git pull --rebase
 # TODO ?
+
+mkdir -p "$USER/Library/Application Support/Google/Chrome/Default/User StyleSheets"
+echo "img { -webkit-transform: rotate(180deg); }" > "$USER/Library/Application Support/Google/Chrome/Default/User StyleSheets/Custom.css"
